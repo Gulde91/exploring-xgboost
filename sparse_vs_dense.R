ba83e7f1dea7f9c68e2221dddfcc7081be5d0168
Scouser008 <alexguldbrand@gmail.com>
2020-11-01 15:31:59 +0100

    Rydder op

--- sparse_vs_dense.R
+++ sparse_vs_dense.R
@@ -68,17 +68,6 @@ models_sparse_tune <- lapply(search_grid,
 tid <- tictoc::toc()
 sparse_vs_dense_result$time_sparse_model <- tid$toc - tid$tic
 
-cv_results_sparse <- models_sparse_tune %>%
-                     bind_rows() %>%
-                    filter(test_auc == max(test_auc))
-best_iter <- cv_results_sparse$nrounds
-cv_best_params <- as.list(dplyr::select(cv_results_sparse, -test_auc, -nrounds))
-
-model_sparse <- xgboost(data = sparse_train, label = response_train,
-                        params = cv_best_params, nrounds = best_iter,
-                        eval_metric = "auc",
-                        verbose = 1,
-                        print_every_n = 20)
 
 # dense data model ----
 dense_train <- as.matrix(data_one_hot_train)
@@ -95,76 +84,4 @@ models_dense_tune <- lapply(search_grid,
 tid <- tictoc::toc()
 sparse_vs_dense_result$time_dense_model <- tid$toc - tid$tic
 
-cv_results_dense <- models_dense_tune %>%
-                    bind_rows() %>%
-                    filter(test_auc == max(test_auc))
-best_iter <- cv_results_dense$nrounds
-cv_best_params <- as.list(dplyr::select(cv_results_dense, -test_auc, -nrounds))
-
-model_dense <- xgboost(data = dense_train, label = response_train,
-                       params = cv_best_params, nrounds = best_iter,
-                       eval_metric = "auc",
-                       verbose = 1,
-                       print_every_n = 20)
-
-# evaluerer modellen på testsæt ----
-pred_sparse <- predict(model_sparse, sparse_test)
-pred_dense <- predict(model_dense, dense_test)
-
-##### side analyse #####
-pred_sparse_tmp <- predict(model_sparse, dense_test)
-identical(pred_sparse_tmp, pred_sparse)
-summary(pred_sparse_tmp)
-summary(pred_sparse)
-
-pred_dense_tmp <- predict(model_dense, sparse_test)
-identical(pred_dense_tmp, pred_dense)
-summary(pred_dense_tmp)
-summary(pred_dense)
-
-# NB! Hvis modellen er trænet på sparse data, så er
-# det vigtigt at den også scorer på sparse data.
-# Ellers giver scoringer ingen mening. Problemet
-# gælder dog ikke den anden vej rundt, hvor en model
-# træner på dense data, godt kan score på sparse
-# data med samme resultater!
-########################
-
-# udregner roc
-auc_sparse <- round(auc(roc(response_test, pred_sparse)), 4)
-auc_dense <- round(auc(roc(response_test, pred_dense)), 4)
-
-pred_list <- list(pred_sparse, pred_dense)
-m <- length(pred_list)
-actuals_list <- rep(list(response_test), m)
-
-pred <- prediction(pred_list, actuals_list)
-rocs <- performance(pred, "tpr", "fpr")
-
-jpeg("./results/sparse_vs_dense_roc_plot.jpg")
-
-plot(rocs, col = as.list(1:m), main = "ROC Curves")
-
-legend(x = "bottomright",
-       legend = c(paste("Sparse model auc:", auc_sparse),
-                  paste("Dense model auc:", auc_dense)),
-       fill = 1:m, cex = 1)
-
-dev.off()
-
-# udregner accuracy
-pred_sparse_class <- ifelse(pred_sparse >= 0.5, 1, 0)
-pred_dense_class <- ifelse(pred_dense >= 0.5, 1, 0)
-
-acc_sparse_model <- sum(pred_sparse_class == response_test) / length(response_test)
-acc_dense_model <- sum(pred_dense_class == response_test) / length(response_test)
-
-cat("Accuracy for sparse model er:", acc_sparse_model, "\n")
-cat("Accuracy for dense model er:", acc_dense_model, "\n")
-
-sparse_vs_dense_result$acc_sparse_model <- acc_sparse_model
-sparse_vs_dense_result$acc_dense_model <- acc_dense_model
-
 save(sparse_vs_dense_result, file = "./results/sparse_vs_dense_result.rda")
-
-
